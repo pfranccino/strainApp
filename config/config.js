75cfe4e09750c7167206799b763e9fc9ecafa6dd
pfranccino <paul.ayala02@inacapmail.cl>
2018-07-09 22:28:22 -0400

    fix yargs

--- config/config.js
+++ config/config.js
@@ -1,22 +1,13 @@
 const argv = require('yargs').options({
 
-    name: {
-        alias: 'n',
-        desc: 'con este parametro ingresamos el nombre de la planta',
-        demand: true
+        name: {
+            alias: 'n',
+            desc: 'con este parametro ingresamos el nombre de la planta',
+            demand: true
 
-    }
-
-})
-
-.options({
-        list: {
-            alias: 'l',
-            desc: 'con este parametro listas el nombre y tipo de toda las plantas',
         }
 
-    })
-    .help()
+    }).help()
     .argv
 
 
